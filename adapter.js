ce5aecdffc0496cdb46615030baf85f95c37b45b
embbnux <embbnux@embbnux.com>
2021-03-09 15:11:59 +0800

    add new call notification in standalong window

--- adapter.js
+++ adapter.js
@@ -5,11 +5,49 @@ window.addEventListener('unload', function() {
   localStorage.removeItem('standalone-widget-opened');
 });
 
+var notificationEnabled = false;
+function requireNotificationPermission() {
+  if (!window.Notification) {
+    console.warn('This browser does not support system notifications.')
+    return;
+  }
+  if (notificationEnabled) {
+    return;
+  }
+  if (window.Notification.permission !== 'denied') {
+    window.Notification.requestPermission(function () {
+      if (window.Notification.permission === 'granted') {
+        notificationEnabled = true;
+      }
+    });
+  }
+}
+
+function createDesktopNotification(options) {
+  if (!notificationEnabled) {
+    console.warn('Notifications is not enabled');
+    return;
+  }
+  const n = new window.Notification(
+    options.title, { body: options.text, icon: options.icon }
+  );
+  n.onclick = options.onClick;
+}
+
 window.addEventListener('message', (e) => {
   const data = e.data;
   if (data) {
     localStorage.setItem('message-transport-key', JSON.stringify(data));
     localStorage.removeItem('message-transport-key');
+  }
+  if (data.type === 'rc-call-ring-notify') {
+    createDesktopNotification({
+      title: 'New Call',
+      text: 'New Call from ' + (data.call.fromUserName || data.call.from),
+      onClick: function() {
+        window.focus();
+      };
+    })
   }
 });
 
@@ -22,3 +60,5 @@ window.addEventListener('storage', function(event) {
     document.querySelector("#rc-widget-adapter-frame").contentWindow.postMessage(JSON.parse(event.newValue), '*');
   }
 });
+
+requireNotificationPermission();
